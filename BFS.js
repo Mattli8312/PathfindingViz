02944b9aa4161302a7272e26e65f45dbbfc080bb
Matthew Lian <mlian5@illinois.edu>
2021-05-16 14:46:47 -0500

    finished implementing BFS

---
+++ BFS.js
@@ -0,0 +1,99 @@
+/**
+ * Simple implementatio of BFS on a 2d grid
+ * @param {*} start_x 
+ * @param {*} start_y 
+ * @param {*} end_x 
+ * @param {*} end_y 
+ * @param {*} frequency_w 
+ * @param {*} frequency_h 
+ */
+async function BFS_Traversal(start_x, start_y, end_x, end_y, frequency_w, frequency_h){
+
+    let curr_x = start_x, curr_y = start_y;
+
+    /**
+     * (-1,-1),(0,-1),(1,-1)
+     * (-1, 0)        (1, 0)
+     * (-1, 1),(0, 1),(1, 1)
+     */
+    let delta_x = [-1,0,1,-1,1,-1,0,1];
+
+    let delta_y = [-1,-1,-1,0,0,1,1,1];
+
+    let BFS_queue = [[curr_x, curr_y]];
+
+    /**
+     * Initialize 2d visited array to zeros 
+     */
+
+    for(var a = 0; a < frequency_h; a++){
+        for(var b = 0; b < frequency_w; b++){
+            document.getElementById(b + ',' + a).setAttribute("visited", "false");
+        }
+    }
+
+    while(BFS_queue.length > 0){
+
+        let curr_path = BFS_queue.splice(0, 1);
+
+        console.log(curr_path)
+
+        curr_x = curr_path[0][0], curr_y = curr_path[0][1]
+
+        let curr_tile = document.getElementById(curr_x + ',' + curr_y)
+
+        if(curr_x != start_x || curr_y != start_y){
+        
+            curr_tile.style.background = "cyan"
+
+        }
+        
+        if(curr_x == end_x && curr_y == end_y){
+
+            break;
+
+        }
+
+        for(var a = 0; a < 8; a++){
+
+            if(curr_x + delta_x[a] < 0 || curr_x + delta_x[a] >= frequency_w){
+                continue;
+            }
+
+            else if(curr_y + delta_y[a] < 0 || curr_y + delta_y[a] >= frequency_h){
+                continue;
+            }
+
+            /**
+             * (-1,-1),(0,-1),(1,-1)
+             * (-1, 0)        (1, 0)
+             * (-1, 1),(0, 1),(1, 1)
+             */
+
+            let neighbor = document.getElementById((curr_x + delta_x[a]) + ',' + (curr_y + delta_y[a]))
+
+            if(neighbor.getAttribute("visited") == "false" && neighbor.getAttribute("type") != "wall"){
+
+                await wait(10);
+
+                BFS_queue.push([curr_x + delta_x[a], curr_y + delta_y[a]]);
+
+                neighbor.setAttribute("visited", "true")
+
+            }
+
+        }
+
+    }
+
+}
+
+async function wait(ms) {
+
+    return new Promise(resolve => {
+
+      setTimeout(resolve, ms);
+
+    });
+
+  }
--- main_app.js
+++ main_app.js
@@ -15,9 +15,16 @@ let current_state = state.DONE_DRAW;
 
 const tile_width = innerWidth * 0.9 * 1/frequency_w;
 
-const tile_height = innerHeight * 0.9 * 1/frequency_h
+const tile_height = innerHeight * 0.8 * 1/frequency_h
+
+/**
+ * body stores reference to grid element
+ * start positions and end positions
+ */
 
 let body = document.getElementById('grid');
+
+let start_x = start_y = end_x = end_y = -1;
 
 function initialize_grid(){
 
@@ -35,17 +42,23 @@ function initialize_grid(){
 
         tile.style.width = tile_width;
 
-        tile.style.height = tile_width;
+        tile.style.height = tile_height;
 
         tile.style.border = "solid black 1px";
 
         tile.setAttribute("type", "space")
+
+        tile.setAttribute("id", j + ',' + i);
+
+        tile.setAttribute("x", j); tile.setAttribute("y", i);
 
         tile.addEventListener('click', ()=>{
 
             if(current_state == state.DRAW_WALLS){
 
                 current_state = state.DRAW_WALL_BREAKS
+
+                tile.setAttribute("type", "wall")
             
             }
             
@@ -56,15 +69,41 @@ function initialize_grid(){
             }
         
             else if(current_state == state.DRAW_START){
+
+                if(start_x != -1 && start_y != -1){
+
+                    let prev_end = document.getElementById(start_x + ',' + start_y);
+
+                    prev_end.style.background = "white";
+
+                    prev_end.setAttribute("type", "space");
+                }
     
-                tile.style.background = "cyan"
+                tile.style.background = "rgb(0,255,127)"
+
+                tile.setAttribute("type", "start")
+
+                start_x = parseInt(tile.getAttribute("x")); start_y = parseInt(tile.getAttribute("y"));
     
                 current_state = state.DRAW_WALL_BREAKS
     
             }
             else if(current_state == state.DRAW_END){
 
-                tile.style.background = "orange"
+                if(end_x != -1 && end_y != -1){
+
+                    let prev_end = document.getElementById(end_x + ',' + end_y);
+
+                    prev_end.style.background = "white";
+
+                    prev_end.setAttribute("type", "space");
+                }
+
+                tile.style.background = "red"
+                
+                end_x = parseInt(tile.getAttribute("x")); end_y = parseInt(tile.getAttribute("y"));
+
+                tile.setAttribute("type", "end")
 
                 current_state = state.DONE_DRAW;
 
@@ -76,6 +115,8 @@ function initialize_grid(){
             if(current_state == state.DRAW_WALLS){
 
                 tile.style.background = "black"
+
+                tile.setAttribute("type", "wall")
 
             }
 
