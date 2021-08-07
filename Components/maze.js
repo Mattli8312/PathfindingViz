b36b18e767dc21bbadf02b36f0808a9b7d704012
Matthew Lian <mlian5@illinois.edu>
2021-08-06 19:42:15 -0500

    updated

--- Components/maze.js
+++ Components/maze.js
@@ -1,3 +1,5 @@
+const wall_color = "black";
+
 class Maze{
     constructor(width, height){
         this.width = width;
@@ -27,27 +29,27 @@ class Maze{
 
 async function RenderMaze(maze_object){
     var wall = document.getElementById("0,0");
-    wall.style.background = "white";
+    wall.style.background = wall_color;
     for(var i = 1; i < maze_object.height * 2 + 1; i+=2){
         for(var j = 1; j < maze_object.width * 2 + 1; j+=2){
             if(maze_object.grid[Math.floor(i/2)][Math.floor(j/2)][0]){
                 wall = document.getElementById(i + ',' + (j+1));
-                wall.style.background = "white";
+                wall.style.background = wall_color;
             }
             if(maze_object.grid[Math.floor(i/2)][Math.floor(j/2)][1]){
                 wall = document.getElementById((i+1) + ',' + j);
-                wall.style.background = "white";
+                wall.style.background = wall_color;
             }
             if(i == 1){
-                wall = document.getElementById((i-1) + ',' + (j)); wall.style.background = "white";
-                wall = document.getElementById((i-1) + ',' + (j+1)); wall.style.background = "white";
+                wall = document.getElementById((i-1) + ',' + (j)); wall.style.background = wall_color;
+                wall = document.getElementById((i-1) + ',' + (j+1)); wall.style.background = wall_color;
             }
             if(j == 1){
-                wall = document.getElementById((i) + ',' + (j-1)); wall.style.background = "white";
-                wall = document.getElementById((i+1) + ',' + (j-1)); wall.style.background = "white";
+                wall = document.getElementById((i) + ',' + (j-1)); wall.style.background = wall_color;
+                wall = document.getElementById((i+1) + ',' + (j-1)); wall.style.background = wall_color;
             }
             wall = document.getElementById((i+1)+','+(j+1));
-            wall.style.background = "white";
+            wall.style.background = wall_color;
             await new Promise(resolve => setTimeout(resolve, 50));
         }
     }
