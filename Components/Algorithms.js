c72cae84e3e5fa238c88fe5d3ff1dae3352f20df
Matthew Lian <mlian5@illinois.edu>
2021-08-09 17:04:32 -0500

    fixed overflow bug with BFS

--- Components/Algorithms.js
+++ Components/Algorithms.js
@@ -3,27 +3,30 @@
  * algorithms used for visualization
  */
 
-
 /**Solves Maze or any graph traversal using Depth First Search or Breadth First Search */
 async function SolveMaze_Traversal(traversal, xi=1,yi=1,xf=2*width-1,yf=2*height-1){
 
     let stack = [[yi,xi,"start"]];
     
     let cx, cy;
+
+    let dfs = traversal == 'DFS';
     //First modify css of the cells
     $('[type=tile]').addClass('animated');
+
+    $('#'+yi+'a'+xi).attr('type','passed');
     
     while(stack.length > 0){
 
-        let curr = stack.splice((traversal == "DFS" ? stack.length-1 : 0),1)[0];
+        let curr = stack.splice(dfs ? stack.length - 1 : 0,1)[0];
 
         let curr_div = $('#'+curr[0]+'a'+curr[1]);
 
         cx = curr[1]; cy = curr[0];
 
-        curr_div.attr('type','passed');
-
         curr_div.attr('prev',curr[2]);
+
+        if(dfs) $('#'+(curr[0])+'a'+(curr[1])).attr('type','passed');
 
         if(curr[0] == yf && curr[1] == xf) break;
 
@@ -31,7 +34,13 @@ async function SolveMaze_Traversal(traversal, xi=1,yi=1,xf=2*width-1,yf=2*height
 
             if(validDivIndx(curr[0]+dy[i]/2,curr[1]+dx[i]/2) && CanPass(curr[1],curr[0],curr[1]+dx[i]/2,curr[0]+dy[i]/2)){
 
-                stack.push([curr[0]+dy[i]/2,curr[1]+dx[i]/2,curr_div.attr('id')]);
+                if($('#'+(curr[0]+dy[i]/2)+'a'+(curr[1]+dx[i]/2)).attr('type') != 'passed'){
+
+                    stack.push([curr[0]+dy[i]/2,curr[1]+dx[i]/2,curr_div.attr('id')]);
+
+                    if(!dfs) $('#'+(curr[0]+dy[i]/2)+'a'+(curr[1]+dx[i]/2)).attr('type','passed');
+
+                }
 
             }
         }
@@ -50,11 +59,11 @@ async function SolveMaze_Dijkstra(xi=1,yi=1,xf=2*width-1,yf=2*height-1){
     let pq = new PriorityQueue();
     let cx, cy;
     pq.push({x:xi,y:yi,val:0,prev:"start"});
-    while(!pq.empty()){
+    $('#'+yi+'a'+xi)
+    while(pq.length){
         let curr = pq.pop();
         let curr_div = $('#'+curr.y+'a'+curr.x);
         let curr_weight = curr.prev != "start" ? curr.val + parseInt($('#' + curr.prev).attr('weight')) : 0;
-        curr_div.attr('type','passed');
         curr_div.attr('prev',curr.prev);
         curr_div.attr('weight', curr_weight);
         cx = curr.x, cy = curr.y;
@@ -70,12 +79,13 @@ async function SolveMaze_Dijkstra(xi=1,yi=1,xf=2*width-1,yf=2*height-1){
                         val:curr_weight+1,
                         prev:curr_div.attr('id')
                     })
+                    curr_div.attr('type','passed');
                 }
             }
         }
         await new Promise(resolve => setTimeout(resolve, 10));
     }
-    if(cx == xf && cy == xy){
+    if(cx == xf && cy == yf){
         await Backtrack(cx,cy);
     }
 }
